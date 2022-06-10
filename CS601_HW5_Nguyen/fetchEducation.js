6b1687070afd9dfbf46eb65de6f95644473cd5f9
Ashley Nguyen <apn2my@virginia.edu>
2022-06-10 11:59:14 -0400

    working

--- CS601_HW5_Nguyen/fetchEducation.js
+++ CS601_HW5_Nguyen/fetchEducation.js
@@ -1,3 +1,49 @@
+const mockData = [
+    {
+        "school": "University of Virginia",
+        "program": "College of Arts & Sciences",
+        "majors": ["Computer Science", "Cognitive Science"],
+        "type": "BA",
+        "year_conferred": "2017"
+    }, 
+    {
+        "school": "Boston University",
+        "program": "Metropolitan College",
+        "majors": ["Software Development"],
+        "type": "MS",
+        "year_conferred": "2023"
+    }
+]
+
+// Function responsible for handling JSON data to render as HTML
+const renderEducationData = (eduData) => {
+    const div = document.getElementById("table-content")
+
+    eduData.forEach( eduObject => {
+        const header = document.createElement("h2")
+        div.appendChild(header)
+        header.textContent = eduObject.school
+
+        for (let [key, value] of Object.entries(eduObject)) {
+            // create elements
+            const details = document.createElement("p")
+            details.className += "details"
+            header.appendChild(details)
+
+            // handle special case for array
+            if (key == "majors") {
+                value = value.join(", ")
+            }
+
+            // format key nicely
+            key = key.toUpperCase().replace("_", " ")
+
+            // set the text in the html
+            details.textContent = `${key} -- ${value}`
+          }
+    })
+}
+
 const fetchEducation = () => {
     fetch("https://warm-fenglisu-170e31.netlify.app/education.json")
         .then(response => { 
@@ -7,17 +53,6 @@ const fetchEducation = () => {
                 return response.json()
             }
         })
-        .then(data => {
-            document.getElementById('container').prepend(data)
-        })
+        .then(data => renderEducationData(data))
         .catch(error => {console.error(error)})
-    // const data = await response.json()
-
-    // data.map((link,i) => {
-    //     var img = new Image();
-    //     img.setAttribute("width", "400px")
-    //     img.setAttribute("height", "auto")
-    //     img.src = link
-    //     document.getElementById('container').prepend(img)
-    // })
 }
